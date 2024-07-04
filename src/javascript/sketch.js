91789b4d32d359f3dbe3c39a2ba088742fca6d4b
Jonas <xgv866@alumni.ku.dk>
2024-07-04 21:51:56 +0200

    update threejs library

--- src/javascript/sketch.js
+++ src/javascript/sketch.js
@@ -21,45 +21,57 @@ scene.add(container);
 var objectLines = new THREE.Object3D();
 scene.add(objectLines);
 
-var loader = new THREE.TextureLoader();
-loader.crossOrigin = 'Anonymous';
+// Create a simple circle texture
+var canvas = document.createElement('canvas');
+canvas.width = 32;
+canvas.height = 32;
+var ctx = canvas.getContext('2d');
+ctx.beginPath();
+ctx.arc(16, 16, 14, 0, 2 * Math.PI);
+ctx.fillStyle = 'white';
+ctx.fill();
+
+var texture = new THREE.CanvasTexture(canvas);
+
 var mat = new THREE.PointsMaterial({
-  color:0x000000,
-  map: loader.load('https://s3-us-west-2.amazonaws.com/s.cdpn.io/127738/dotTexture.png'),
+  color: 0x000000,
+  map: texture,
   transparent: true,
   alphaTest: 0.5,
   sizeAttenuation: false
 });
+
 var lines = [];
 var dotsPerLine = 250;
 var amountLines = 500;
 
 function Line(){
-  this.geometry = new THREE.Geometry();
+  this.geometry = new THREE.BufferGeometry();
+  var positions = new Float32Array(dotsPerLine * 3);
+  for(var i = 0; i < dotsPerLine; i++){
+    positions[i * 3] = (i - dotsPerLine / 2) * 0.4;
+    positions[i * 3 + 1] = 0;
+    positions[i * 3 + 2] = 0;
+  }
+  this.geometry.setAttribute('position', new THREE.BufferAttribute(positions, 3));
   this.mesh = new THREE.Points(this.geometry, mat);
   this.length = Math.floor(Math.random() * 100 + 250);
   this.speed = Math.random() * 400 + 200;
-  for(var i=-(this.length*0.5);i<(this.length*0.5);i++){
-    var vector = new THREE.Vector3(i*0.4, 0, 0);
-    this.geometry.vertices.push(vector);
-  }
   this.mesh.rotation.x = Math.random() * Math.PI;
   this.mesh.rotation.y = Math.random() * Math.PI;
   this.mesh.rotation.z = Math.random() * Math.PI;
 }
+
 Line.prototype.update = function(a) {
-  for(var i=0;i<this.geometry.vertices.length;i++){
-    var vector = this.geometry.vertices[i];
-    vector.y = Math.sin(a/this.speed + i*0.1) * 2.2;
+  var positions = this.geometry.attributes.position.array;
+  for(var i = 0; i < dotsPerLine; i++){
+    positions[i * 3 + 1] = Math.sin(a/this.speed + i*0.1) * 2.2;
   }
-  
-  this.geometry.verticesNeedUpdate = true;
+  this.geometry.attributes.position.needsUpdate = true;
 };
 
-
 function init() {
-  
-  for(var i=0;i<amountLines;i++) {
+  for(var i=0; i<amountLines; i++) {
     lines.push(new Line());
     objectLines.add(lines[i].mesh);
   }
@@ -68,19 +80,18 @@ function init() {
 
   window.addEventListener("resize", onResize);
 }
+
 function render(a){
   requestAnimationFrame(render);
-  
   
   objectLines.rotation.y = (a * 0.0001);
   objectLines.rotation.x = (-a * 0.0001);
 
-  for(var i=0;i<amountLines;i++) {
+  for(var i=0; i<amountLines; i++) {
     lines[i].update(a);
   }
   renderer.render(scene, camera);
 }
-
 
 function onResize() {
   ww = window.innerWidth;
@@ -90,4 +101,10 @@ function onResize() {
   renderer.setSize(ww, wh);
 }
 
-init();
+// Check if WebGL is available
+if (THREE.WEBGL.isWebGLAvailable()) {
+  init();
+} else {
+  var warning = THREE.WEBGL.getWebGLErrorMessage();
+  document.body.appendChild(warning);
+}
